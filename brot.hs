c4ef9675c93cddbdb864cfd7142ebc33de097093
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-06-03 13:49:53 +0200

    a lil' bit of code tweaking

--- brot.hs
+++ brot.hs
@@ -1,6 +1,7 @@
 module Main where
 import System.Environment
 import System.Console.ANSI
+import Control.Monad
 import Data.Char
 
 data Screen = Screen Int Int
@@ -22,8 +23,9 @@ arg args i = read $ args !! i
 showBrot :: Screen -> String -> IO ()
 showBrot _ [] = return ()
 showBrot scr @ (Screen w _) str = do 
-    putStrLn $ take w str
+    when (any (' ' /=) line) $ putStrLn line
     showBrot scr $ drop w str
+    where line = take w str
 
 createBrot :: Screen -> String
 createBrot s @ (Screen w h) = 
@@ -40,6 +42,6 @@ intensity x y s @ (Screen w h) = iteration 0 0 sx sy 0 10
 
 iteration :: Float -> Float -> Float -> Float -> Int -> Int -> Int
 iteration x y sx sy i mi 
-    | x*x + y*y < 2*2 && i < mi = 
-        iteration (x*x - y*y + sx) (2*x*y + sy) sx sy (i+1) mi
+    | x^2 + y^2 < 2*2 && i < mi = 
+        iteration (x^2 - y^2 + sx) (2*x*y + sy) sx sy (i+1) mi
     | otherwise = i
