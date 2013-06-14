8690adb96bd09fd8eb40d9ab8b574a69dc82aa65
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-06-14 14:47:26 +0200

    added comments + small refactoring

--- brot.hs
+++ brot.hs
@@ -4,49 +4,60 @@ import System.Console.ANSI
 import Control.Monad
 import Data.Char
 
-data Screen = Screen Int Int
+data Size       = Size Int Int
+type Mandelbrot = [Int]
 
+-- given a width and height as parameters 
+-- prints a colored ASCII mandelbrot 
 main :: IO ()
 main = do
     args <- getArgs
     if length args /= 2 then
         putStrLn "Usage: brot width height"
     else do
-        let s = Screen (arg args 0) (arg args 1)
+        let s = Size (arg args 0) (arg args 1)
         showBrot s $ mandelbrot s
         setSGR []
 
+-- read a specific argument
 arg :: Read a => [String] -> Int -> a
 arg args i = read $ args !! i
 
-showBrot :: Screen -> [Int] -> IO ()
+-- display the given mandelbrot
+showBrot :: Size -> Mandelbrot -> IO ()
 showBrot _ [] = return ()
-showBrot scr @ (Screen w _) str = do 
+showBrot scr @ (Size w _) str = do 
     showLine line
     showBrot scr rest
     where (line, rest) = splitAt w str
 
+-- display a single line of the mandelbrot
 showLine :: [Int] -> IO ()
 showLine []     = putStrLn ""
-showLine (x:xs) = setColor x >> putChar (ascii x) >> showLine xs
+showLine (x:xs) = setColor x >> putChar $ ascii x >> showLine xs
 
+-- given an intensity, sets a specific color
 setColor :: Int -> IO ()
 setColor i = setSGR [SetColor Foreground Vivid $ colors !! i] 
     where colors = [Black .. White] ++ [Red .. Yellow]
 
+-- translate an intensity to a character
 ascii :: Int -> Char
 ascii i = chr $ ord ' ' + 10 - i
 
-mandelbrot :: Screen -> [Int]
-mandelbrot s @ (Screen w h) = 
+-- given a Size, create a mandelbrot
+mandelbrot :: Size -> Mandelbrot
+mandelbrot s @ (Size w h) = 
     [intensity x y s | y <- [0 .. h-1], x <- [0 .. w-1]]
 
-intensity :: Int -> Int -> Screen -> Int
-intensity x y s @ (Screen w h) = iteration 0 0 sx sy 0 10
+-- compute intensity of the point at (x, y)
+intensity :: Int -> Int -> Size -> Int
+intensity x y s @ (Size w h) = iteration 0 0 sx sy 0 10
     where 
     sx = -2.5 + 3.5 * fromIntegral x / fromIntegral w
     sy = -1 + 2 * fromIntegral y / fromIntegral h
 
+-- helper function for intensity
 iteration :: Double -> Double -> Double -> Double -> Int -> Int -> Int
 iteration x y sx sy i mi 
     | x^2 + y^2 < 2*2 && i < mi = 
