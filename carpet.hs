bc403e0e30d1ed9ea7f4270d5502d2561884e8ae
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-06-20 00:34:34 +0200

    refactoring + added some comments

--- carpet.hs
+++ carpet.hs
@@ -1,12 +1,13 @@
 module Main where
 import System.Environment
-import System.Console.ANSI
 import Safe (readMay)
 
 data Size   = Size Int Int
 type Tile   = Bool
 type Carpet = [Tile]
 
+-- given width and height as arguments
+-- prints an ascii sirpinski carpet
 main :: IO ()
 main = do
     args <- getArgs
@@ -14,33 +15,37 @@ main = do
         putStrLn "Usage: carpet <width> <height>"
     else do
         let maybeSize = do
-            w <- readMay $ args !! 0
+            w <- readMay $ head args
             h <- readMay $ args !! 1
             return $ Size w h
         maybeCarpet maybeSize
 
+-- if arguments are valid, create and show carpet
 maybeCarpet :: Maybe Size -> IO ()
 maybeCarpet Nothing  = putStrLn "You've provided an invalid argument"
 maybeCarpet (Just s) = showCarpet s $ carpet s
 
+-- prints a carpet to the screen
 showCarpet :: Size -> Carpet -> IO ()
 showCarpet _ []             = return ()
 showCarpet s @ (Size w _) c = do
-                            mapM (putStr . showTile) line
+                            mapM_ (putChar . showTile) line
                             putStrLn ""
                             showCarpet s rest
     where (line, rest) = splitAt w c
 
-showTile :: Tile -> String
-showTile True  = "#"
-showTile False = " "
+-- convert Tile to String
+showTile :: Tile -> Char
+showTile True  = '#'
+showTile False = ' '
 
+-- create a Carpet of given Size
 carpet :: Size -> Carpet
 carpet (Size w h) = [isInCarpet x y | y <- [1..h], x <- [1..w]]
 
+-- check whether a point (x, y) is in the Carpet
 isInCarpet :: Int -> Int -> Bool
 isInCarpet x y
-    | x > 0 || y > 0 = if (x `mod` 3 == 1) && (y `mod` 3 == 1)
-                       then False
-                       else isInCarpet (x `div` 3) (y `div` 3)
+    | x > 0 || y > 0 = not ((x `mod` 3 == 1) && (y `mod` 3 == 1))
+                       && isInCarpet (x `div` 3) (y `div` 3)
     | otherwise      = True
